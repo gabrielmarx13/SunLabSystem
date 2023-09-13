4b5f4fc252d64b672957f212e2bece8e05f38fce
gabrielmarx13 <gabrielmarx13@gmail.com>
2023-09-13 09:00:31 -0400

    Removed comments and default menu

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
     app.quit();
 }
 
 const createWindow = async () => {
-    // Create the browser window.
+
     const mainWindow = new BrowserWindow({
         width: 1080,
         height: 720,
@@ -17,10 +16,10 @@ const createWindow = async () => {
         }
     });
 
-    // and load the index.html of the app.
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-    //mainWindow.webContents.openDevTools();
+    mainWindow.setMenu(null);
+
 
     // See existical's fix for this active Electron issue https://github.com/electron/electron/issues/31917#issuecomment-1061142818
     ipcMain.on('focus-fix', () => {
@@ -29,14 +28,8 @@ const createWindow = async () => {
     });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
@@ -44,12 +37,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
